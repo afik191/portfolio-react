0463fb80ccc313c7c3db773e18005b5b119086ca
Afik191 <afik3248@gmail.com>
2026-05-15 13:56:06 +0300

    change resume and projects

--- my-app/src/projects.jsx
+++ my-app/src/projects.jsx
@@ -11,16 +11,16 @@ const projectsData = [
     liveDemoLink: "#",
     status: "done",
   },
-  {
-    title: "Real-Time Location Sharing App", 
-    description:
-      "A full-stack web app for real-time location sharing in groups, Users can create accounts, join groups, and share live locations with others. Still in active development.",
-    image: "/realtime_location_app.png", 
-    tech: ["React", "Node.js", "Express", "MongoDB", "Socket.IO", "Cloudinary"],
-    githubLink: "https://github.com/afik191/realtime-location-app",
-    liveDemoLink: "https://realtime-location-app.onrender.com",
-    status: "in-progress",
-  },
+  // {
+  //   title: "Real-Time Location Sharing App", 
+  //   description:
+  //     "A full-stack web app for real-time location sharing in groups, Users can create accounts, join groups, and share live locations with others. Still in active development.",
+  //   image: "/realtime_location_app.png", 
+  //   tech: ["React", "Node.js", "Express", "MongoDB", "Socket.IO", "Cloudinary"],
+  //   githubLink: "https://github.com/afik191/realtime-location-app",
+  //   liveDemoLink: "https://realtime-location-app.onrender.com",
+  //   status: "in-progress",
+  // },
   {
     title: "Intelligent robotic arm", 
     description:
